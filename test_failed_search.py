e1651bb85ef2754c8528af174b5bbefb93e187b8
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-09-07 18:53:14 +0300

    Add test for empty search result

---
+++ test_failed_search.py
@@ -0,0 +1,6 @@
+from selene import browser, be, have
+
+def test_failed_search (set_browser):
+    browser.config.timeout = 8
+    browser.element('[id="searchbox_input"]').should(be.blank).type('Xx9abT72kLmQ').press_enter()
+    browser.all('h2 a').should(have.size(0))
