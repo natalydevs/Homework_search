77cbe68b1e13b6dc3030097783dd2f81c85a7332
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-09-07 18:55:06 +0300

    Update global fixture settings

--- conftest.py
+++ conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from selene import browser
 
-@pytest.fixture()
-def set_browser(scope="function"):
+@pytest.fixture(scope="function")
+def set_browser_resolution():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+@pytest.fixture(scope="function")
+def set_browser(set_browser_resolution):
     browser.open("https://duckduckgo.com")
