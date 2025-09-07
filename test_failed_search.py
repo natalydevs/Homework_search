4ace8b54dc7ce4a13ee486912fb91144c32a1bcd
Nataly Kostiuchenko <natcheshirecats@yandex.ru>
2025-09-07 19:03:37 +0300

    Small update: add print line

--- test_failed_search.py
+++ test_failed_search.py
@@ -4,3 +4,4 @@ def test_failed_search (set_browser):
     browser.config.timeout = 8
     browser.element('[id="searchbox_input"]').should(be.blank).type('Xx9abT72kLmQ').press_enter()
     browser.all('h2 a').should(have.size(0))
+    print("Нет результатов поиска")
